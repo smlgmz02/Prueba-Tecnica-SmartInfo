00d6ec383f7ced5419c61e06b20cea5a141ac33c
Samuel <sagole2006gmail.com>
2026-08-25 16:48:31 -0500

    docs: adicion del script para la base de datos junto a la creación del controlador principal.

---
+++ database.sql
@@ -0,0 +1,16 @@
+-- SCRIPT DE LA BASE DE DATOS -- 
+
+CREATE DATABASE GestorContactos;
+USE GestorContactos;
+
+CREATE TABLE IF NOT EXISTS Contactos(
+    id_contacto INT AUTO_INCREMENT PRIMARY KEY, /* Se añade un ID autoincremental para mantener la consistencia de los registros en caso de realizar consultas */
+    nombre_completo VARCHAR(255) NOT NULL,
+    correo_electronico VARCHAR(150) NOT NULL,
+    telefono_contacto VARCHAR(20) NOT NULL -- Tipo varchar porque sigue siendo una cadena de caracteres con los que no se puede operar.
+)
+
+-- INSERCIONES (DATOS DE PRUEBA) --
+
+INSERT INTO Contactos VALUES ("Samuel Gomez", "samuelgomez@gmail.com", "3152297732"),
+INSERT INTO Contactos VALUES ("Lucia Rodriguez", "luciarodriguez@gmail.com", "3205257732"),
